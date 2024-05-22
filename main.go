a31b29a1a44d7b5f25ffce7b1c6a3222c2b94a9b
Pau Guerrero <pauguerrero@gmail.com>
2024-05-22 19:02:56 +0200

    post!

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/mattn/go-mastodon"
 )
@@ -35,5 +36,40 @@ func main() {
 	}
 	for i := len(timeline) - 1; i >= 0; i-- {
 		fmt.Println(timeline[i])
+	}
+
+	post, err := LoadPost()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var media mastodon.Media
+	file, err := os.Open("./posts/" + post.Asset)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	media = mastodon.Media{File: file, Description: post.AssetAlt}
+	attachment, err := c.UploadMediaFromMedia(context.Background(), &media)
+	var attachmentIDs []mastodon.ID
+
+	attachmentIDs = append(attachmentIDs, attachment.ID)
+
+	finalText := post.Text + "\n"
+
+	for i := 0; i < len(post.Tags); i++ {
+		finalText = finalText + "#" + post.Tags[i] + " "
+	}
+
+	toot := mastodon.Toot{
+		Status:   finalText,
+		MediaIDs: attachmentIDs,
+	}
+
+	fmt.Printf("About to publish: %#v\n", toot)
+
+	c.PostStatus(context.Background(), &toot)
+	if err != nil {
+		log.Fatalf("%#v\n", err)
 	}
 }
